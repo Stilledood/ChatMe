d23f2de3fb642a0e9286a3578447599cd71bbf9b
Florin <74854275+Stilledood@users.noreply.github.com>
2022-12-01 17:08:15 +0200

    Created user app and added models for each user and for user profile

--- chatter/user/models.py
+++ chatter/user/models.py
@@ -1,56 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from autoslug import AutoSlugField
 from PIL import Image
 from django.shortcuts import reverse
-
-
-class CustomUser(AbstractUser):
-    '''Class to create a custom user class'''
-
-    age = models.PositiveInteger(null =True, blank= True)
-    friends = models.ManyToManyField('CustomUser', blank =True)
+from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    '''Class to construct a model for each CustomUser instance'''
+    '''Class to construct a profile model connected to each user instance'''
 
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg',upload_to='profile_pics')
-    slug = AutoSlugField(populate_from='user', null=True)
-    description = models.TextField(max_length=250, default='')
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='defautl.jpg', upload_to='profile_pics')
+    name = models.CharField(max_length=256)
+    username = models.CharField(max_length=128)
+    email_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.user.username}'s Profile"
+        return f"{self.username}'s Profile"
 
     def get_absolute_url(self):
-        return reverse('user_profile',kwargs={'slug':self.slug})
+        return reverse('user_profile', kwargs={'username' : self.username})
 
     def get_update_url(self):
-        return reverse('user_profile_update', kwargs={'slug':self.slug})
+        return reverse('user_profile_details', kwargs={'username':self.username})
 
-    def save(self,*args, **kwargs):
-        super(Profile,self).save(*args, **kwargs)
-        img = Image.open(self.image.path)
-        if img.height > 400 or img.width > 400:
-            output_size = (400,400)
-            img.thumbnail(output_size)
-            img.save((self.image.path))
-
-
-# Function to automatically create a profile when a user signup
-@receiver(post_save, sender=CustomUser)
-def create_profile(sender,instance, created, **kwargs):
+# Function to create a user profile when a user signup
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-# Function to save the profile after user signup
-@receiver(post_save, sender=CustomUser)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
 
 
 
