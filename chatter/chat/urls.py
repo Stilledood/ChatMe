aa32ec0f97d32a406b7c73b13a6fc9a9f7fede1b
Florin <74854275+Stilledood@users.noreply.github.com>
2022-12-07 19:07:25 +0200

    Created utl pattern for CreateRoom

--- chatter/chat/urls.py
+++ chatter/chat/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("messenger/", views.DefaultChatRoomList.as_view(), name ='index'),
     path("messenger/chat_room/<str:room_name>/", views.ConnectToRoom.as_view(), name='room'),
     path('messenger/categories/', views.CategoriesList.as_view(), name='categories'),
+    path('messenger/rooms/create/',views.CreateRoomView.as_view(), name='create_room'),
 ]
