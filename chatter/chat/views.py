0d97b261bbc24c5b5145189cdd33224535e46fdd
Florin <74854275+Stilledood@users.noreply.github.com>
2022-12-10 18:23:17 +0200

    Created  the view -to create or acces a Private chat room between 2 users

--- chatter/chat/views.py
+++ chatter/chat/views.py
@@ -4,8 +4,10 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.views.generic import View
 from .forms import ChatRoomForm
+from django.http import Http404
 
 
+# Public Room Views
 class DefaultChatRoomList(View):
     '''Class to create a view to display all available chat rooms'''
 
@@ -81,6 +83,42 @@ class DeleteRoomView(View):
         room = get_object_or_404(self.model, name=room_name)
         room.delete()
         return redirect('messenger')
+
+
+# Private Room Views
+
+class PrivateRoomView(View):
+    '''Class to create a view to display/create a private room between 2 users'''
+
+    template_user1 = 'chat/userchat1.html'
+    template_user2 = 'chat/userchat2.html'
+    model = models.PrivateChatRoom
+
+    def get(self,request,pk):
+        current_user = request.user
+        other_user = User.objects.get(pk=pk)
+
+        if not self.model.objects.filter(user1==current_user, user2=other_user).exists() and not self.model.objects.filter(user1=other_user,user2=current_user).exists():
+            room = self.model.objects.create(user1=current_user, user2=other_user)
+            return render(request, self.template_name, context={'room':room})
+
+        elif self.model.objects.filter(user1=current_user, user2=other_user).exists():
+            room = self.model.get(user1=current_user, user2=other_user)
+            chat_messages = []
+            if current_user == room.user1:
+                chat_messages = models.PrivateMessage.objects.filter(private_room=room)
+                return render(request, self.template_user1,context={'room': room,
+                                                                    'chat_messages': chat_messages})
+        elif self.model.objects.filter(user1=other_user,user2=current_user).exists():
+            room = self.model.get(user1=other_user, user2=current_user)
+            chat_messages = []
+            if current_user == room.user2:
+                chat_messages = models.PrivateMessage.objects.filter(private_room=room)
+                return render(request, self.template_user2, context={'room':room,
+                                                                     'chat_messages':chat_messages})
+
+        else:
+            raise Http404('User not authorized for this chatroom')
 
 
 
