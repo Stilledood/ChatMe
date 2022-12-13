96b394d5ef1bc6fb00710926a28903bcdba02d76
Florin <74854275+Stilledood@users.noreply.github.com>
2022-12-13 17:03:19 +0200

    Created AllRoomsListView url pattern

--- chatter/chat/urls.py
+++ chatter/chat/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path("messenger/", views.DefaultChatRoomList.as_view(), name ='messenger'),
     path('messenger/categories/', views.CategoriesList.as_view(), name ='categories'),
+    path('messenger/all_rooms/', views.AllRoomsView.as_view(), name='all_rooms'),
     path("messenger/chat_room/<str:room_name>/", views.ConnectToRoom.as_view(), name='room'),
     path('messenger/chat_room/<str:room_name>/details/', views.RoomDetailsView.as_view(), name='room_details'),
     path('messenger/chat_room/<str:room_name>/disconnect/',views.RoomDisconnect.as_view(), name='disconnect'),
