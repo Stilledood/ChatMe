a0beb37f031b50408bbb8c0665311816bbf99abe
Florin <74854275+Stilledood@users.noreply.github.com>
2022-12-13 19:36:12 +0200

    Created a custom permission  required( a decorator that can be applied on a class)

--- chatter/user/customdecorators.py
+++ chatter/user/customdecorators.py
@@ -10,3 +10,14 @@ def custom_login_decorator(cls):
     decorator = method_decorator(login_required())
     cls.dispatch = decorator(cls.dispatch)
     return cls
+
+def custom_permission_required(permission):
+
+    def decorator(cls):
+        if not isinstance(cls,type) or not issubclass(cls,View):
+            raise ImproperlyConfigured('custom permission required decorator must be apllied on View subclass')
+        check_auth = method_decorator(login_required)
+        check_perm = method_decorator(permission_required(permission,raise_exception=True))
+        cls.dispatch = check_auth(check_perm(cls.dispatch))
+        return cls
+    return decorator
