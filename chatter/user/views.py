a720c3ea709b0c172cc8e4f9ecf084cb0bb4b093
Florin <74854275+Stilledood@users.noreply.github.com>
2022-12-14 16:43:54 +0200

    Created a view to allow a user to delete his/her account

--- chatter/user/views.py
+++ chatter/user/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import SignUpForm, ProfileForm
 from .models import Profile
 from django.contrib.auth import get_user, logout
 from django.conf import settings
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect
+from django.utils.decorators import method_decorator
+
 
 
 
@@ -16,5 +20,30 @@ class UserProfile(View):
     def get(self,request,username):
         profile =  self.model.objects.get(username = username)
         return render(request,self.template,context={'user':profile})
+
+
+class DisableUserAccount(View):
+    '''Class to create a view to allow a user to delete his/her account'''
+    template_name = 'user/disable_account.html'
+
+    @method_decorator(login_required)
+    @method_decorator(csrf_protect)
+    def get(self,request):
+        return render(request,self.template_name)
+
+    @method_decorator(login_required)
+    @method_decorator(csrf_protect)
+    def post(self,request):
+        user = request.user
+        user.set_unusable_password = True
+        user.is_active = False
+        user.save()
+        logout(request)
+        return redirect('messenger')
+
+
+
+
+
 
 
