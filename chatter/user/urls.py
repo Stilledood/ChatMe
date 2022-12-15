385cc67b07688b2e5844c65a04c4bc02d05334a0
Florin <74854275+Stilledood@users.noreply.github.com>
2022-12-15 16:42:17 +0200

    Added url pattern and templates for sign up process

--- chatter/user/urls.py
+++ chatter/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import re_path, path,include
 from django.views.generic import RedirectView
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import AuthenticationForm
-from .views import UserProfile,DisableUserAccount
+from .views import UserProfile,DisableUserAccount,SingUpView
 from django.urls import reverse_lazy
 
 
@@ -24,6 +24,7 @@ urlpatterns = [
     re_path(r'^login/$',auth_views.LoginView.as_view(template_name='user/login.html', authentication_form=AuthenticationForm), name='login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(template_name='user/logout.html', extra_context={'form':AuthenticationForm}),name ='logout'),
     path('disable_account/', DisableUserAccount.as_view(), name='disable_account'),
+    path('signup/', SingUpView.as_view(), name='signup'),
     path('<str:username>/',UserProfile.as_view(),name='profile'),
     path('password/',include(password_urlpatterns)),
 ]
