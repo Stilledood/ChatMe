61a57bcfb8ab7ca0bea551b9916ef358ef063463
Florin <74854275+Stilledood@users.noreply.github.com>
2022-12-15 16:24:38 +0200

    Created a view for user sign up process

--- chatter/user/views.py
+++ chatter/user/views.py
@@ -8,6 +8,12 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect
 from django.utils.decorators import method_decorator
 from django.template.response import TemplateResponse
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_str,force_bytes
+from .tokens import account_activation
+from django.contrib import messages
 
 
 
@@ -43,6 +49,37 @@ class DisableUserAccount(View):
         user.save()
         logout(request)
         return redirect('messenger')
+
+class SingUpView(View):
+    '''Class to create a view for signup process'''
+
+    form_class = SignUpForm
+    template_name = 'user/signup.html'
+
+    def get(self,request):
+        return render(request,self.template_name,context={'form':self.form_class()})
+
+    def post(self,request):
+        bound_form = self.form_class(request.POST)
+        if bound_form.is_valid():
+            user = bound_form.save(commit=False)
+            user.is_active = False
+            user.save()
+            Profile.objects.update_or_create(user=user, defaults={'username':user.get_username()})
+            site = get_current_site(request)
+            subject = "Activate your account"
+            message = render_to_string('user/account_activation_email.html',
+                                       {
+                                           'user':user,
+                                           'domain':site.domain,
+                                           'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                                           'token':account_activation.make_token(user)
+                                       })
+            user.email_user(subject,message)
+            messages.success('Please check your email')
+            return redirect('dj-auth:login')
+        else:
+            return render(request,self.template_name,context={'form':bound_form})
 
 
 
