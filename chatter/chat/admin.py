c04a1c55b4afe8bd5537b2e1f670600957c29652
Florin <74854275+Stilledood@users.noreply.github.com>
2022-12-16 10:51:17 +0200

    Created a custom Admin Model class for RoomCategory modell

--- chatter/chat/admin.py
+++ chatter/chat/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import RoomCategory, ChatRoom, PrivateChatRoom
 from django.db.models import Count
 
-admin.site.register(RoomCategory)
 admin.site.register(PrivateChatRoom)
+
 
 @admin.register(ChatRoom)
 class ChatRoomAdmin(admin.ModelAdmin):
@@ -29,9 +29,20 @@ class ChatRoomAdmin(admin.ModelAdmin):
     def online_members(self,chatroom):
         return chatroom.members
     online_members.short_description = 'Members'
-    
 
 
+@admin.register(RoomCategory)
+class RoomCategoryAdmin(admin.ModelAdmin):
+    '''Class to create a custom admin model for RoomCategory model'''
+
+    list_display = ('name', 'slug', 'room_number')
+    list_filter = ('name', 'slug')
+    search_fields = ('name',)
+
+
+    def room_number(self,category):
+        return category.chatroom_set.count()
+    room_number.short_description = 'Rooms'
 
 
 
