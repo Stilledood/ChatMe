aebd796147092bd65a1b504a6c179537bf40b1e2
Florin <74854275+Stilledood@users.noreply.github.com>
2022-12-21 18:57:47 +0200

    Created a a new javascript function to allow multiple users to join a videocall

--- chatter/static/js/videochat/streams.js
+++ chatter/static/js/videochat/streams.js
@@ -3,19 +3,17 @@ const CHANNEL = 'main'
 const TOKEN = '007eJxTYKir5zRxv6brOmXu1PTG996fTjGoTMk+qiqffKXcY0cYt4YCg4FxSoqFWaqFiYlJkkmSRZKlRXKKWZKlmXGyZZJxoqUZ48eFyQ2BjAy6XRMZGRkgEMRnYchNzMxjYAAAuuAdeA=='
 let UID;
 
-const client = AgoraRTC.create_client({mode:'rtc', codec:'vp8'})
+const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
 
 let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async() => {
+    client.on('user-published', handleUserJoined)
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
-    localTracks await AgoraRTC.createMicrophoneAndCameraTracks()
-    let player = `<div class="video-container" id="user-container-${UID}">
-                    <div class="video-player" id="user-${UID}"></div>
-                    <div class="username-wrapper"><span class="user-name">My Name</span></div>
-                </div>`
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
+    let player = `<div class="video-container" id="user-container-${UID}"><div class="video-player" id="user-${UID}"></div><div class="username-wrapper"><span class="user-name">My Name</span></div></div>`
     
     document.getElementById("video-streams").insertAdjacentHTML('beforeend',player)
     
@@ -24,8 +22,32 @@ let joinAndDisplayLocalStream = async() => {
     await client.publish([localTracks[0],localTracks[1]])
 
 
-}
+};
+let handleUserJoined = async (user,mediaType) => {
+    remoteUsers[user.id] = user
+    await client.subscribe(user, mediaType)
+
+    if(mediaType === 'video'){
+        let player =  document.getElementById(`user-container-${user.uid}`)
+        if(player != null){
+            player.remove()
+        };
+        player = `<div class="video-container" id="user-container-${user.uid}"><div class="username-wrapper"><span class="user-name">My Name</spam></div><div class="video-player" id="user-${user.uid}"></div></div>`
+
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend',player)
+        user.videoTrack.play(`user-${user.uid}`)
+
+    };
+
+    if(mediaType === 'audio'){
+        user.audioTrack.play()
+    };
+
+
+};
+
 
 joinAndDisplayLocalStream()
+handleUserJoined()
 
 
