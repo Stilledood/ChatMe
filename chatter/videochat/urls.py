ba0a1f592dd91a7dd2d8addc9a0334c3becbe006
Florin <74854275+Stilledood@users.noreply.github.com>
2022-12-22 19:27:43 +0200

    Connected create_member and getMember views to frontend part

--- chatter/videochat/urls.py
+++ chatter/videochat/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.videochatlobbyviews, name='lobby'),
     path('room/', views.videochatroomviews, name='room'),
     path('get_token/', views.getToken),
-    path('get_member/', views.getMember),
+    path('create_member/', views.createMember),
+    path('room/get_member/', views.getMember),
     path('delete_member/', views.deleteMember)
 ]
