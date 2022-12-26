63fdf6177addd6e56ad0f673cbb3fc6b3515cdb3
Florin <74854275+Stilledood@users.noreply.github.com>
2022-12-26 19:01:01 +0200

    Connected delete member -to remove a chat member when the member press leave room button or close the browser

--- chatter/static/js/videochat/streams.js
+++ chatter/static/js/videochat/streams.js
@@ -71,6 +71,7 @@ let leaveAndRemoveLocalStream = async() =>{
     }
 
     await client.leave()
+    deleteMember()
     window.open('/','_self')
 };
 
@@ -116,8 +117,22 @@ let getMember = async (user) =>{
     return member
 };
 
+let deleteMember = async () =>{
+    let response = await fetch('delete_member/',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({'name': NAME, 'room_name':CHANNEL,'UID':UID})
+    })
+
+    let member = await response.json()
+
+}
+
 joinAndDisplayLocalStream()
 
+window.addEventListener('beforeunload',deleteMember)
 document.getElementById('leave-btn').addEventListener('click',leaveAndRemoveLocalStream)
 document.getElementById('camera-btn').addEventListener('click',toggleCamera)
 document.getElementById('mic-btn').addEventListener('click', toogleMicrophone)
